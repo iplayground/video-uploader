9fe0ef8838b7ef739f60b434a5c9d72dc078cbae
Superbil <superbil@gmail.com>
2019-11-24 19:13:59 +0800

    Setup request google drive
    Don't work for find driveID... WTF

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ from google.auth.transport.requests import Request
 import youtube_uploader
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
-          'https://www.googleapis.com/auth/drive.readonly']
+          'https://www.googleapis.com/auth/drive.readonly',
+          'https://www.googleapis.com/auth/drive.metadata.readonly']
 
 load_dotenv(verbose=True)
 SHEET_ID = os.getenv('GSHEET_ID')
 SHEET_RANGE = os.getenv('GSHEET_RANGE')
+GDRIVE_ID = os.getenv('GDRIVE_ID')
 
 
 def main():
@@ -41,22 +43,32 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
+    # Setup Sheet
     g_sheet_service = build('sheets', 'v4', credentials=creds)
     sheet = g_sheet_service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SHEET_ID,
                                 range=SHEET_RANGE).execute()
-    values = result.get('values', [])
+    sheet_values = result.get('values', [])
 
-    if not values:
+    # Setup GDrive
+    g_drive_service = build('drive', 'v3', credentials=creds)
+    results = g_drive_service.files().list(
+        fields='nextPageToken, files(id, name)',
+        includeItemsFromAllDrives=True,
+        driveId=GDRIVE_ID,
+    ).execute()
+    g_drive_values = results.get('files', [])
+
+    if not sheet_values:
         print('No data found.')
     else:
         print('Sheet Values:')
-        for row in values:
+        for row in sheet_values:
             print(row)
-        # for row in values:
-        #     # Print columns A and E, which correspond to indices 0 and 4.
-        #     print('%s, %s' % (row[0], row[4]))
 
+        print('GDrive list:')
+        for item in g_drive_values:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
 
 if __name__ == '__main__':
     main()
